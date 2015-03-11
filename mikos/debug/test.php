1f02e87ba09322450933ed6e6c2a5ccd28cef8b7
florian <bosselmann@silpion.de>
2015-03-11 13:27:16 +0100

    removed legacy stuff

--- mikos/debug/test.php
+++
@@ -1,4 +0,0 @@
-<?php
-$str = '<name>B��ffel Volker</name>';
-echo $str;
-echo utf8_decode($str);
--- mikos/files/logout.php
+++
@@ -1,8 +0,0 @@
-<?
-require_once('header.inc.php');
-
-$_SESSION['mikos']->logout();
-
-session_destroy();
-header('Location: http://web2.baggerware.com/index.php?page=627');
-?>
